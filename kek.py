e1bfc842976d2d5a6b150a7744ca947a99dcd52f
Imperator8582 <slas1207@gmail.com>
2018-12-24 18:54:25 +0300

    delete govna

--- kek.py
+++ kek.py
@@ -1,25 +1,10 @@
 a=[]
 print('Введите n')
 n=int(input())
-#print('Введите m')
-#m=int(input())
-# print('Введите числа')
-# for i in range (n):
-#   #  for j in range (m):
-#        # a[i][j]=int(input())
-#     a.append([int(j) for j in input().split()])  
-# maxim=-1000000
-# for j in range (len(a[i])):
-#     summa=0
-#     for i in range(n):
-#         summa=summa+a[i][j]
-#     if summa > maxim:
-#         maxim=summa
-         
-# print('Наибольшая сумма эелемнтов столбцов= ', maxim)
-# print(a)
+
 for i in range(len(a)):
     print(a[i])
 a,b = b,a
 for i in a:
     print(i)
+print('fghfdsagf')
