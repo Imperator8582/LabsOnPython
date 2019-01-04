580397c6f16f6f1c0dd84dfe8afec9eb54f333da
Imperator8582 <slas1207@gmail.com>
2019-01-05 00:38:04 +0300

    ПРавка

--- Laba3_Tyo.py
+++ Laba3_Tyo.py
@@ -9,11 +9,6 @@ n=1
 
 if len(a)> len(set(a)):
     b=True
-#while n<len(a):
-#    for i in range (len(a)-n):
-#        if a[i]==a[i+1]:
-#            b=b+1
- #   n=n+1
 
 if b==True:
     while n<len(a):
