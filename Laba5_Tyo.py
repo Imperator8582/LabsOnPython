f8e6c4eff04f16c02e155f382e630f86a6594897
Stevel <sasha30.07.98@gmail.com>
2019-01-30 23:21:55 +0200

    Fix Lab5

--- Laba5_Tyo.py
+++ Laba5_Tyo.py
@@ -1,32 +1,25 @@
-#5.	Дана последовательность натуральных чисел A=j=1..n (n<=10000). 
-#Удалить из последовательности числа , сумма цифр которых кратна 8, а среди оставшихся 
-#продублировать простые числа 
+# 5.	Дана последовательность натуральных чисел A=j=1..n (n<=10000).
+# Удалить из последовательности числа , сумма цифр которых кратна 8, а среди оставшихся
+# продублировать простые числа
 import math
 print('Введите массив:')
-a=[int(x) for x in input().split()]
+a = [int(x) for x in input().split()]
 
 
-def prost (x):
-    k=0
-    for i in range(sqrt(x)):
-        if x%i==0:
-            k+=1
-    return(k)
+def is_prime(a):
+    for i in range(2, a):
+        if a % i == 0:
+            return False
+    return True
 
 i = 0
 while i < len(a):
-	if a[i]%8==0 and a[i]!=0:
-		del a[i]
-	elif prost(a[i])==0:
-        a.insert(i+1,a[i])
-        i+=2
+    if a[i] % 8 == 0 and a[i] != 0:
+        del a[i]
+    if is_prime(a[i]):
+        a.insert(i+1, a[i])
+        i += 2
     else:
         i+=1
-
-
 print(a)
-print()
-    
-
-
 
